cafaf0a377921be4a64cc961da9be91a24f3499d
Prakash Paudel <total.prakash@gmail.com>
2016-04-21 21:29:50 -0500

    rearranged projects

--- app/projects.js
+++ app/projects.js
@@ -6,10 +6,16 @@ export default [
 		description: "This is the fourth iteration of a website I've built for LumenEd. This version was built using Yii, a PHP MVC framework as the foundation. The frontend also includes Bootstrap, JQuery, other related libraries and a bunch of custom HTML/CSS/JS."
 	},
 	{
-		title: "Weather App",
-		img: "/assets/img/weather-app.png",
-		url: "http://prakashpaudel.github.com/weather-app",
-		description: "This was a just-for-fun project I made to learn React, a JS library that is amazing for building web applications. Weather Worldwide is a single-page web app that uses the OpenWeather API to show the daily and 5 day forecast for anywhere in the world."
+		title: "This Site",
+		img: "/assets/img/portfolio.png",
+		url: "http://prakashpaudel.github.io",
+		description: "TBD"
+	},
+	{
+		title: "Acoustik Musik",
+		img: "/assets/img/am.png",
+		url: "http://www.acoustikmusik.com/",
+		description: "TBD"
 	},
 	{
 		title: "Video Pen Pals",
@@ -19,21 +25,15 @@ export default [
 		description: "Video Pen Pals is a platform for connecting classrooms across the world through video. It's a fully featured Web App with video sharing, messaging, notifications and user management. It's built with PHP, MySQL, HTML, CSS(LESS), JS, WebSocket, Bootstrap, and other related tools. You can check it out using a guest account: lumened.guest@gmail.com - password: guest"
 	},
 	{
-		title: "Acoustik Musik",
-		img: "/assets/img/am.png",
-		url: "http://www.acoustikmusik.com/",
-		description: "TBD"
-	},
-	{
 		title: "The Synapse Magazine",
 		img: "/assets/img/synapse.png",
 		url: "http://www.thesynapsemagazine.org/",
 		description: "TBD"
 	},
 	{
-		title: "This Site",
-		img: "/assets/img/portfolio.png",
-		url: "http://prakashpaudel.github.io",
-		description: "TBD"
-	},
+		title: "Weather App",
+		img: "/assets/img/weather-app.png",
+		url: "http://prakashpaudel.github.com/weather-app",
+		description: "This was a just-for-fun project I made to learn React, a JS library that is amazing for building web applications. Weather Worldwide is a single-page web app that uses the OpenWeather API to show the daily and 5 day forecast for anywhere in the world."
+	}
 ]
