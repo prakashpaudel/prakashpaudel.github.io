6969c277fbbbd581dcf9762074ce4d37b320d384
Prakash Paudel <ppaudel@oberlin.edu>
2016-04-27 22:47:00 +0000

    changed about text

--- app/components/Main.js
+++ app/components/Main.js
@@ -38,7 +38,7 @@ class Main extends Component {
 					<h3 className="heading">About</h3>
 					<div className="section about-section">
 						<img src="/assets/img/pp.jpg" className="profile-pic img-circle" />
-						<p className="">Hey there! I'm a frontend developer based in Chicago. Until recently I was working on my own <a href="http://lumened.org" target="_blank">startup</a>. I graduated with honors from Oberlin College, Ohio in Math, Economics and CS. Today, I'm looking for opportunities to grow my frontend skills until my next startup adventure.</p>
+						<p className="">Hey there! I'm a frontend developer based in Chicago. Until recently I was working on my own <a href="http://lumened.org" target="_blank">startup</a>. I graduated with honors from Oberlin College, Ohio in Math, Economics and CS. Today, I'm looking for my next adventure.</p>
 					</div>
 					<h3 className="heading">Work</h3>
 					<div className="section portfolio-section">
