2601073c462fe127d1f4a03fb967804af8a0a822
Prakash Paudel <total.prakash@gmail.com>
2018-05-10 17:08:48 +0100

    changing deployment to hugo

--- README.md
+++
@@ -1,8 +0,0 @@
-# My Portfolio Site
-A quick page I built to showcase the work I've done and the tools I use/
-
-## Sections
-  - About
-  - Work
-  - Technology
-  - Contact
--- app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './containers/App'
-import './sass/Root.scss'
-
-render(
-	<App />,
-	document.getElementById('app')
-)
